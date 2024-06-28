9f81fafb027b99ace70749b0e698aae09f2a915a
aneelaoad <aneelaoadd@gmail.com>
2024-06-29 00:13:57 +0500

    event registration form added

--- force-app/main/default/lwc/regEventRegister/regEventRegister.js
+++ force-app/main/default/lwc/regEventRegister/regEventRegister.js
@@ -36,24 +36,25 @@ export default class RegEventRegister extends LightningElement {
     handleSubmit() {
         const eventData = {
             eventTitle: this.eventTitle,
-            eventOrganizer: this.eventOrganizer,
-            eventOrganizerCompany: this.eventOrganizerCompany,
+            // eventOrganizer: this.eventOrganizer,
+            // eventOrganizerCompany: this.eventOrganizerCompany,
             eventStartDateTime: this.eventStartDateTime,
             eventEndDateTime: this.eventEndDateTime,
-            eventType: this.eventType,
-            eventCharges: this.eventCharges,
-            eventDescription: this.eventDescription,
-            bannerImage: this.bannerImage,
-            eventStreet: this.eventStreet,
-            eventCity: this.eventCity,
-            eventState: this.eventState,
-            eventPostalCode: this.eventPostalCode,
-            eventCountry: this.eventCountry,
+            // eventDate: this.eventDate,
+            // eventType: this.eventType,
+            // eventCharges: this.eventCharges,
+            // eventDescription: this.eventDescription,
+            // bannerImage: this.bannerImage,
+            // eventStreet: this.eventStreet,
+            // eventCity: this.eventCity,
+            // eventState: this.eventState,
+            // eventPostalCode: this.eventPostalCode,
+            // eventCountry: this.eventCountry,
         };
         console.log('eventData: ', JSON.stringify(eventData));
 
-        registerEvent({ eventData: eventData })
-        // registerEvent({ eventData: JSON.stringify(eventData) })
+        // registerEvent({ eventData: eventData })
+        registerEvent({ eventData: JSON.stringify(eventData) })
             .then(() => {
                 this.dispatchEvent(
                     new ShowToastEvent({
@@ -75,7 +76,19 @@ export default class RegEventRegister extends LightningElement {
                 );
             });
     }
+    // formatDatetime(datetimeString) {
+    //     const date = new Date(datetimeString);
+    //     const pad = (num) => num.toString().padStart(2, '0');
+    //     const year = date.getUTCFullYear();
+    //     const month = pad(date.getUTCMonth() + 1);
+    //     const day = pad(date.getUTCDate());
+    //     const hours = pad(date.getUTCHours());
+    //     const minutes = pad(date.getUTCMinutes());
+    //     const seconds = pad(date.getUTCSeconds());
+    //     const milliseconds = date.getUTCMilliseconds().toString().padStart(3, '0');
 
+    //     return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}+0000`;
+    // }
     resetForm() {
         this.eventTitle = '';
         this.eventOrganizer = '';
